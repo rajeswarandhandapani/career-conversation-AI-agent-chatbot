214f379eca37c4eb570980d5c013bc30558ca99e
Rajeswaran Dhandapani <rajeswaran.dhandapani@gmail.com>
2025-05-18 17:44:16 -0400

    gradio deployment

---
+++ chatbot/README.md
@@ -0,0 +1,6 @@
+---
+title: career-conversation-AI-agent-chatbot
+app_file: app.py
+sdk: gradio
+sdk_version: 5.29.1
+---
--- chatbot/app.py
+++ chatbot/app.py
@@ -78,13 +78,13 @@ class Me:
     def __init__(self):
         self.openai = OpenAI()
         self.name = "Rajeswaran Dhandapani"
-        reader = PdfReader("chatbot/my-profile/linkedin.pdf")
+        reader = PdfReader("my-profile/linkedin.pdf")
         self.linkedin = ""
         for page in reader.pages:
             text = page.extract_text()
             if text:
                 self.linkedin += text
-        with open("chatbot/my-profile/summary.txt", "r", encoding="utf-8") as f:
+        with open("my-profile/summary.txt", "r", encoding="utf-8") as f:
             self.summary = f.read()
 
 
